9a35e9ea7eb193eca69e016f092ac6c9554fe247
Saurav2129737 <sauravshirvastav831@gmail.com>
2024-09-24 14:16:29 +0530

    first commit

---
+++ src/components/WorkoutSessions.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+        A top workout session combines a dynamic warm-up, strength training, high-intensity interval training (HIIT), 
+        and a cool-down. It focuses on major muscle groups, boosts cardiovascular health, and improves flexibility. 
+        Consistency, proper form, and hydration are key for maximum benefits. Tailor workouts to individual fitness 
+        levels for optimal results.!
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+        Bootstrap features responsive design, pre-built components, customizable templates, extensive documentation,
+         grid system, JavaScript plugins, and cross-browser compatibility.?
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>A well-rounded workout routine includes a mix of cardiovascular exercises, strength training,.</h4>
+            <p>
+            Incorporate high-intensity interval training (HIIT) for efficient calorie burning and cardiovascular health. Include 
+            exercises like squats, lunges, push-ups, and planks for full-body engagement!
+            </p>
+          </div>
+          <div>
+            <h4>Fitness Freak GYM .</h4>
+            <p>
+              Pro Unlimited gym !
+            </p>
+          </div>
+          <div>
+            <h4>A gym is a fitness center with equipment and classes..</h4>
+            <p>
+            Consistency, proper technique, and hydration are crucial for maximizing benefits and achieving fitness goals!
+            </p>
+          </div>
+          <div>
+            <h4>A gym offers equipment, classes, and space for various workouts..</h4>
+            <p>
+            A gym provides equipment, classes, and professional guidance for strength training, cardio, and flexibility to improve overall fitness.!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;
